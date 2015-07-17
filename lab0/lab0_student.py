07b4d4d0963f36f05c2c5be0d54daa43160e4a6e
Yueming Sun <cynthia0828@gmail.com>
2015-07-16 23:06:51 -0400

    Update lab0_student.py

--- lab0/lab0_student.py
+++ lab0/lab0_student.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # #![Spark Logo](http://spark-mooc.github.io/web-assets/images/ta_Spark-logo-small.png) + ![Python Logo](http://spark-mooc.github.io/web-assets/images/python-logo-master-v3-TM-flattened_small.png)
